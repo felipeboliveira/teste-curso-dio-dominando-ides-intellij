88b8de6bf366684ec371817a92105fd8bfd68954
Felipe B Oliveira <felipe10059.fb@gmail.com>
2022-01-28 14:33:53 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroProgramajava.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroProgramajava {
+    public static void main(String[] args) {
+        Gato gato = new Gato ();
+
+        System.out.println(gato);
+
+
+        /*int a = 5;
+        int b = 2;
+        System.out.println("Hello World " + (a+b));*/
+
+    }
+}
